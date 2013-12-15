6f2d49d5d94fbbfa959b59e6a35c2c7b4c63d04f
ekianjo <ekianjo@gmail.com>
2013-12-15 15:49:02 +0900

    Update bagomagui.py

--- bagomagui.py
+++ bagomagui.py
@@ -1,2 +1,13 @@
 import PyZenity
+import subprocess
 
+a=PyZenity.InfoMessage("BaGoMa is a tool which will backup your Gmail Emails, so that you can restore or consult them in case the worst happens. Since it may take a while, you can also decide to stop this and restart it later. It will pick up the transfers where it stopped")
+
+login=PyZenity.GetText("First provide your full email login (including @gmail.com)")
+password=PyZenity.GetText("Please provide your password",password=True)
+
+proceed=PyZenity.Question("Ok to proceed right now?")
+
+if proceed==True:
+  cmd="python 
+  subprocess.Popen(
