5ef437021d68a2fcb30daac317acd1fdc09da464
ekianjo <ekianjo@gmail.com>
2014-02-10 11:05:58 +0900

    Update bagomagui.py
    corrected indentation, added support for Cancel buttons.

--- bagomagui.py
+++ bagomagui.py
@@ -11,6 +11,13 @@ def Run(command):
         universal_newlines=True)
     return proc
 
+def Exiting(message):
+    if message=="":
+	    a=PyZenity.InfoMessage("You decided not to proceed at this point. Exiting Now")
+    else:
+    	a=PyZenity.InfoMessage("{0}".format(message))
+    exit
+
 def Trace(proc):
     global found
 	
@@ -21,39 +28,43 @@ def Trace(proc):
             print 'Read line', line
             
             if "Invalid credentials" in line:
-				PyZenity.InfoMessage("The credentials you provided do not match. Exiting now.")
-				exit
+            	PyZenity.InfoMessage("The credentials you provided do not match. Exiting now.")
+		exit
             
             m = re.search('Retrieving (.+?) message', line)
             if m:
-				found = m.group(1)
-				print found
+            	found = m.group(1)
+            	print found
 				
             if found!=0:
-				n=re.search(' (.+?)/{0}'.format(found), line)
-				
-				if n:
-					messagedl=n.group(1)
-					print messagedl
-					progress=100*(float(messagedl)/float(found))
-					print progress
-					proczenity.stdin.write("{0}\n".format(progress))
-					proczenity.stdin.write("# Downloading Message {0} out of {1}\n".format(messagedl,found))
+            	n=re.search(' (.+?)/{0}'.format(found), line)
+            	if n:
+			messagedl=n.group(1)
+			print messagedl
+			progress=100*(float(messagedl)/float(found))
+			print progress
+			proczenity.stdin.write("{0}\n".format(progress))
+			proczenity.stdin.write("# Downloading Message {0} out of {1}\n".format(messagedl,found))
 					
-            
-
 a=PyZenity.InfoMessage("BaGoMa is a tool which will backup your Gmail Emails, so that you can restore or consult them in case the worst happens. Since it may take a while, you can also decide to stop this and restart it later. It will pick up the transfers where it stopped. Note that the IMAP mode must be ON in your GMail settings for this application to work.")
 
 login=PyZenity.GetText("First provide your full email login (including @gmail.com)")
+if login=="None":
+    Exiting("")
+
 password=PyZenity.GetText("Please provide your password",password=True)
+if password=="None":
+    Exiting("You failed to provide any password. Exiting Now.")
 
 proceed=PyZenity.Question("Ok to proceed right now?")
 
 if proceed==True:
-	  cmd="./bagoma.py -e {0} -p {1}".format(login,password)
-	  print cmd
-	  command=["python","bagoma.py","-e",login,"-p",password]
-	  
+    cmd="./bagoma.py -e {0} -p {1}".format(login,password)
+    print cmd
+    command=["python","bagoma.py","-e",login,"-p",password]
+else:
+    Exiting("You decided not to proceed at this point. Exiting Now")
+    
 
 proc = Run(command)
 cmd = 'zenity --progress --text="Backing Up Emails" --auto-close'
